4a80d351f24663dd43e1e5b641a2217233439662
Leonardo Nobrega <leonobr@fastmail.com>
2025-10-25 17:54:08 +0000

    FloatField
    Basically a clone of IntegerField.

---
+++ frontend/packages/pydantic-forms/src/components/fields/FloatField.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import _ from 'lodash';
+
+import type { PydanticFormControlledElementProps } from '@/types';
+import { getFormFieldIdWithPath } from '@/utils';
+
+export const FloatField = ({
+    value,
+    onChange,
+    onBlur,
+    disabled,
+    pydanticFormField,
+}: PydanticFormControlledElementProps) => {
+    // If the field is part of an array the value is passed in as an object with the field name as key
+    // this is imposed by react-hook-form. We try to detect this and extract the actual value
+    const fieldName = getFormFieldIdWithPath(pydanticFormField.id);
+    const fieldValue =
+        _.isObject(value) && _.has(value, fieldName)
+            ? _.get(value, fieldName)
+            : value;
+
+    return (
+        <input
+            data-testid={pydanticFormField.id}
+            onBlur={onBlur}
+            onChange={(t) => {
+                const value = parseFloat(t.currentTarget.value);
+                onChange(value);
+            }}
+            disabled={disabled}
+            value={fieldValue}
+            type="number"
+            style={{
+                padding: '8px',
+                margin: '8px 0',
+            }}
+        />
+    );
+};
